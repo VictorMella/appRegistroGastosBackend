24df33785090df46373e4d18e164fb2919165469
Victor <v.mellaq@gmail.com>
2021-12-16 17:29:58 -0300

    Ajustes

--- cuentas/config.js
+++ cuentas/config.js
@@ -4,6 +4,6 @@ exports.config = {
     // base: 'mongodb+srv://AdminMella:carola7810@cluster0.09jiw.mongodb.net/gastosBD?retryWrites=true&w=majority' || 'mongodb://localhost:27017/gastosBD',
     serverport: Number(process.env.PORT) || 3000,
     // db: 'mongodb://localhost:27017/gastosBD',
-    db: `mongodb+srv://AdminMella:carola7810@cluster0.09jiw.mongodb.net/gastosBD?retryWrites=true&w=majority` || 'mongodb://localhost:27017/gastosBD',
+    db: `mongodb+srv://AdminMella:carola7810@cluster0.09jiw.mongodb.net/gastosBD?retryWrites=true&w=majority`,
     SECRETORPRIVATEKEY: 'E5M1CAV3'
 };
